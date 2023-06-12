0f4d2f478ebd1edce9b70b57683ee15488acc3fb
Melanie Silies <melanie.silies@web.de>
2023-06-12 13:44:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ const customer = {
 
 function App() {
   //your code inside these <div>
-  return <div></div>;
+  return <div>
+    <h1>My name is {customer.first_name}</h1>
+    <h2>My last name is {customer.last_name}</h2>
+  </div>;
 }
 
 export default App;
